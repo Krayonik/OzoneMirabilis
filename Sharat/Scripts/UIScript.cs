396bcc5edce68e46cbe64900684de1ec91ab177f
Sharatchandra Aithal <xeon.base@gmail.com>
2016-03-22 17:58:02 +0530

    UI Additions

---
+++ Sharat/Scripts/UIScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIScript : MonoBehaviour {
+
+    public Animator Amen_Animator;
+
+    bool Amen_State;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void ToggleAmenPanel()
+    {
+        if(Amen_State)
+        {
+            Amen_Animator.SetBool("Open", false);
+        }
+        else
+        {
+            Amen_Animator.SetBool("Open", true);
+        }
+        Amen_State = !Amen_State;
+    }
+
+
+}
---
+++ Sharat/Scripts/UiCompass.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UiCompass : MonoBehaviour {
+
+    public GameObject Target;
+
+    public RectTransform _Self;
+
+    public float Smootness = 0.001f;
+
+	// Use this for initialization
+	void Start () {
+
+        _Self = this.GetComponent<RectTransform>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        
+
+        var v = Target.transform.forward;
+        v.y = 0;
+        v.Normalize();
+
+        if (Vector3.Angle(v, Vector3.forward) <= 45.0)
+        {
+            Debug.Log("North");
+        }
+        else if (Vector3.Angle(v, Vector3.right) <= 45.0)
+        {
+            Debug.Log("East");
+        }
+        else if (Vector3.Angle(v, Vector3.back) <= 45.0)
+        {
+            Debug.Log("South");
+        }
+        else
+        {
+            Debug.Log("West");
+        }
+
+        Debug.Log(Vector3.Angle(v, Vector3.forward));
+
+        //_Self.rotation.SetEulerAngles(new Vector3(0, 0, Vector3.Angle(v, Vector3.forward)));
+
+        //_Self.localRotation.SetEulerAngles(new Vector3(0, 0, Vector3.Angle(v, Vector3.forward)));
+
+        //_Self.localEulerAngles.Set(0,0,Vector3.Angle(v, Vector3.forward));
+
+        _Self.rotation = Quaternion.EulerAngles(0, 0, Vector3.Angle(v, Vector3.forward) * Smootness);
+	}
+}
