d7e01906f327a7b9131ec18b0969723a0bdb2265
Sharatchandra Aithal <xeon.base@gmail.com>
2016-03-27 12:44:04 +0530

    Drag Behaviour Patch

---
+++ Sharat/Scripts/DragBehaviour.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class DragBehaviour : MonoBehaviour, IBeginDragHandler, IEndDragHandler
+{
+
+
+    public KGFOrbitCam Cam;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnBeginDrag()
+    {
+
+    }
+
+    public void OnEndDrag()
+    {
+
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+
+        Cam.isMoveable = false;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+
+        Cam.isMoveable = true;
+    }
+}
