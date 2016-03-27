9f7b429a156a385a28d47718ca4cbd6c05b10fea
Sharatchandra Aithal <xeon.base@gmail.com>
2016-03-27 23:01:50 +0530

    Changes in CD

--- Sharat/Scripts/CameraDirector.cs
+++ Sharat/Scripts/CameraDirector.cs
@@ -7,10 +7,27 @@ public class CameraDirector : MonoBehaviour {
     public KGFOrbitCamSettings CityViewSettings;
     public KGFOrbitCamSettings ProjectViewSettings;
 
+
+    public GameObject VR_Button;
+    public GameObject Back_Button;
+
+    public enum MenuState
+    {
+        City,
+        Project,
+        Apartment
+    };
+
+    public MenuState CurrState;
+
 	// Use this for initialization
 	void Start () {
 
         CityViewSettings.Apply();
+        VR_Button.SetActive(false);
+        Back_Button.SetActive(false);
+
+        CurrState = MenuState.City;
 
 	}
 
@@ -18,13 +35,29 @@ public class CameraDirector : MonoBehaviour {
     public void OnClick_CityView()
     {
         CityViewSettings.Apply();
+        VR_Button.SetActive(false);
+        Back_Button.SetActive(false);
+        CurrState = MenuState.City;
     }
 
     public void OnClick_ProjectView()
     {
         ProjectViewSettings.Apply();
+        VR_Button.SetActive(true);
+        Back_Button.SetActive(true);
+        CurrState = MenuState.Project;
     }
 	
+    public void OnClick_Back()
+    {
+        switch(CurrState)
+        {
+            case MenuState.Project:
+                OnClick_CityView();
+                break;
+        }
+    }
+
 	// Update is called once per frame
 	void Update () {
 	
