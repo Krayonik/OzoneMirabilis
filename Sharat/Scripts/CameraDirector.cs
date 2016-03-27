7ec85a0297f79c90b797cfb9d8ebd5044490aeec
Sharatchandra Aithal <xeon.base@gmail.com>
2016-03-27 14:02:20 +0530

    Camera Director Added

---
+++ Sharat/Scripts/CameraDirector.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraDirector : MonoBehaviour {
+
+
+    public KGFOrbitCamSettings CityViewSettings;
+    public KGFOrbitCamSettings ProjectViewSettings;
+
+	// Use this for initialization
+	void Start () {
+
+        CityViewSettings.Apply();
+
+	}
+
+
+    public void OnClick_CityView()
+    {
+        CityViewSettings.Apply();
+    }
+
+    public void OnClick_ProjectView()
+    {
+        ProjectViewSettings.Apply();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
