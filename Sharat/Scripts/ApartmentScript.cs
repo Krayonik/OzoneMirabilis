9f4f7a5ac03f695dc049d9b4b02f75d4df45ebe6
Sharatchandra Aithal <xeon.base@gmail.com>
2016-03-28 18:07:53 +0530

    Changes to WAFN01TY01

---
+++ Sharat/Scripts/ApartmentScript.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ApartmentScript : MonoBehaviour {
+
+    public GameObject FlyCam;
+    public GameObject WalkCam;
+
+    public GameObject Roof;
+
+	// Use this for initialization
+	void Start () {
+
+
+        FlyMode();
+
+	}
+	
+    public void WalkMode()
+    {
+        Roof.SetActive(true);
+
+        WalkCam.SetActive(true);
+        FlyCam.SetActive(false);
+    }
+
+    public void FlyMode()
+    {
+        Roof.SetActive(false);
+
+        WalkCam.SetActive(false);
+        FlyCam.SetActive(true);
+    }
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
