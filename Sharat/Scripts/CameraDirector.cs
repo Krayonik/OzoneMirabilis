7880f273fbdc531e98fd19be96e3642ce96ddb31
Sharatchandra Aithal <xeon.base@gmail.com>
2016-03-28 13:19:35 +0530

    CD Changes

--- Sharat/Scripts/CameraDirector.cs
+++ Sharat/Scripts/CameraDirector.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class CameraDirector : MonoBehaviour {
 
@@ -11,6 +12,17 @@ public class CameraDirector : MonoBehaviour {
     public GameObject VR_Button;
     public GameObject Back_Button;
 
+
+    public CanvasGroup LocationsUI;
+    public CanvasGroup WorldspaceUI;
+
+    public Image City_Button;
+    public Image Project_Button;
+    public Image Apartment_Button;
+
+    public Color Inactive_Color;
+    public Color Active_Color;
+
     public enum MenuState
     {
         City,
@@ -18,15 +30,92 @@ public class CameraDirector : MonoBehaviour {
         Apartment
     };
 
-    public MenuState CurrState;
+    private MenuState currState;
+
+    public MenuState CurrState
+    {
+        get { return currState; }
+        set
+        {
+            currState = value;
+            switch(currState)
+            {
+                case MenuState.City:
+                    Event_CityView();
+                    break;
+                case MenuState.Project:
+                    Event_ProjectView();
+                    break;
+                case MenuState.Apartment:
+                    Event_ApartmentView();
+                    break;
+            }
+        }
+    }
+
+
+    IEnumerator LocationUIChange(bool direction)
+    {
+        float SeedValue = 1;
+
+        if(direction)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                LocationsUI.alpha += 0.1f;
+                WorldspaceUI.alpha += 0.1f;
+                yield return new WaitForSeconds(Time.deltaTime * SeedValue);
+            }
+        }
+        else
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                LocationsUI.alpha -= 0.1f;
+                WorldspaceUI.alpha -= 0.1f;
+                yield return new WaitForSeconds(Time.deltaTime * SeedValue);
+            }
+        }
+        
+    }
+
+    void Event_CityView()
+    {
+        StartCoroutine("LocationUIChange", true);
+
+        City_Button.color = Active_Color;
+        Project_Button.color = Inactive_Color;
+        Apartment_Button.color = Inactive_Color;
+
+    }
+
+    void Event_ProjectView()
+    {
+        StartCoroutine("LocationUIChange", false);
+
+        City_Button.color = Inactive_Color;
+        Project_Button.color = Active_Color;
+        Apartment_Button.color = Inactive_Color;
+    }
+
+    void Event_ApartmentView()
+    {
+
+    }
 
 	// Use this for initialization
 	void Start () {
 
+        Active_Color = new Color(0.2f, 0.388f, 0.47f, 1);
+        Inactive_Color = City_Button.color;
+
+        LocationsUI.alpha = 0;
+        WorldspaceUI.alpha = 0;
+
         CityViewSettings.Apply();
         VR_Button.SetActive(false);
         Back_Button.SetActive(false);
-
+        
         CurrState = MenuState.City;
 
 	}
