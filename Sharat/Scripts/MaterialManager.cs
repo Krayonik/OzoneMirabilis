d0b19a214170a76ab571fe4423a6d7ca74d3715f
Sharatchandra Aithal <xeon.base@gmail.com>
2016-03-31 22:07:01 +0530

    Mat Manager

---
+++ Sharat/Scripts/MaterialManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MaterialManager : MonoBehaviour {
+
+    [SerializeField]
+    public Renderer Roof1;
+    [SerializeField]
+    public Renderer Roof2;
+    [SerializeField]
+    public Renderer Roof3;
+
+    [SerializeField]
+    public Renderer Wall;
+
+
+    //Retro
+    [SerializeField]
+    public Material Retro_Roof1Mat;
+    [SerializeField]
+    public string[] Retro_Walls;
+
+	// Use this for initialization
+	void Start () {
+
+        Roof1.material = Retro_Roof1Mat;
+        Roof2.material = Retro_Roof1Mat;
+        Roof3.material = Retro_Roof1Mat;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Sharat/Scripts/MaterialManagerEditor.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+
+[CustomEditor(typeof(MaterialManager))]
+public class MaterialManagerEditor : Editor {
+
+
+    bool MainMat_bool;
+    bool RetroGroup_bool;
+
+    private bool listVisibility = true;
+    private string entry;
+    
+
+    public override void OnInspectorGUI()
+    {
+        
+        MaterialManager myTarget = (MaterialManager)target;
+
+        SerializedObject serOBJ;
+
+        
+
+        serOBJ = new SerializedObject(target);
+        serOBJ.Update();
+
+        //myTarget.experience = EditorGUILayout.IntField("Experience", myTarget.experience);
+        //EditorGUILayout.LabelField("Level", myTarget.Level.ToString());
+
+        MainMat_bool = EditorGUILayout.BeginToggleGroup("Main Materials", MainMat_bool);
+
+        myTarget.Roof1 = (Renderer) EditorGUILayout.ObjectField("Roof 1 :",myTarget.Roof1, typeof(Renderer), true);
+        myTarget.Roof2 = (Renderer) EditorGUILayout.ObjectField("Roof 2 :", myTarget.Roof2, typeof(Renderer), true);
+        myTarget.Roof3 = (Renderer) EditorGUILayout.ObjectField("Roof 3 :", myTarget.Roof3, typeof(Renderer), true);
+
+        EditorGUILayout.EndToggleGroup();
+
+        RetroGroup_bool = EditorGUILayout.BeginToggleGroup("Retro Materials", RetroGroup_bool);
+        myTarget.Retro_Roof1Mat = (Material)EditorGUILayout.ObjectField("Roof Mat :", myTarget.Retro_Roof1Mat, typeof(Renderer), true);
+        
+
+        EditorGUILayout.EndToggleGroup();
+    }
+
+
+    
+
+
+}
+
