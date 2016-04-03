b376c49f5a1f5d6e45a23cdede3012f763f1bcea
Sharatchandra Aithal <xeon.base@gmail.com>
2016-04-03 22:52:45 +0530

    Adding Material Manager Window

---
+++ Sharat/Scripts/MatManagerWindow.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class MatManagerWindow : EditorWindow {
+
+
+    [MenuItem("EyE-RiS/MaterialManager")]
+    public static void ShowWindow()
+    {
+        EditorWindow.GetWindow(typeof(MatManagerWindow));
+    }
+
+    void OnGUI () {
+            // The actual window code goes here
+           }
+
+
+
+
+}
+
+
+
