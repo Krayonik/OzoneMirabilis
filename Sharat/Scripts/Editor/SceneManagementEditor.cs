d66628f3462f86af7ceba2c27c94c5ed0af61f42
Sharatchandra Aithal <xeon.base@gmail.com>
2016-04-09 17:15:00 +0530

    Scene Management

---
+++ Sharat/Scripts/Editor/SceneManagementEditor.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.IO;
+
+public class SceneManagementEditor : EditorWindow {
+
+
+    public static string[] Scenes;
+
+    [MenuItem("EyE-RiS/SceneManager")]
+    public static void ShowWindow()
+    {
+        EditorWindow.GetWindow(typeof(SceneManagementEditor));
+        Start();
+    }
+
+	// Use this for initialization
+	public static void Start () {
+
+        
+
+        
+
+        
+
+	}
+
+    void OnEnable()
+    {
+        Scenes = Directory.GetFiles(Application.dataPath + "/OzoneMirabilis/Scenes", "*.unity");
+    }
+
+    void OnGUI()
+    {
+
+        char[] delimiters = {'/'};
+        for (int i = 0; i < Scenes.Length; i++)
+        {
+            string[] StringToBeFed = Scenes[i].Split(delimiters);
+            if (GUILayout.Button(StringToBeFed[5]))
+            {
+                Application.LoadLevel(Scenes[i]);
+            }
+        }
+
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
