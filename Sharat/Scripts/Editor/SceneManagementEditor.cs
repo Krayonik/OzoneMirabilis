ff78527f918d27d478b96d2957a34f5c139cb046
Sharatchandra Aithal <xeon.base@gmail.com>
2016-04-10 18:33:30 +0530

    SME Update

--- Sharat/Scripts/Editor/SceneManagementEditor.cs
+++ Sharat/Scripts/Editor/SceneManagementEditor.cs
@@ -36,6 +36,8 @@ public class SceneManagementEditor : EditorWindow {
     void OnGUI()
     {
 
+        EditorGUILayout.LabelField("Scenes : ");
+
         char[] delimiters = {'/'};
         for (int i = 0; i < Scenes.Length; i++)
         {
@@ -46,6 +48,41 @@ public class SceneManagementEditor : EditorWindow {
             }
         }
 
+        EditorGUILayout.LabelField("Patches : ");
+        if (GUILayout.Button("Heirarchy Patch"))
+        {
+            GameObject DebugObject = new GameObject("DebugObject");
+            DebugObject.layer = 8;
+            DebugObject.tag = "DebugObject";
+            Transform DebugObjectTranform = DebugObject.transform;
+            GameObject DLight = GameObject.Find("Directional Light");
+            GameObject ESystem = GameObject.Find("EventSystem");
+            GameObject Cam = GameObject.Find("KGFOrbitCam");
+            GameObject Canvas = GameObject.Find("Canvas");
+            GameObject FPS = GameObject.Find("FPSController");
+
+            DLight.transform.SetParent(DebugObjectTranform);
+            ESystem.transform.SetParent(DebugObjectTranform);
+            Cam.transform.SetParent(DebugObjectTranform);
+            Canvas.transform.SetParent(DebugObjectTranform);
+            FPS.transform.SetParent(DebugObjectTranform);
+
+            GameObject LevelObject = new GameObject(SceneManager.GetActiveScene().name);
+            LevelObject.tag = "LevelObject";
+            GameObject[] RootObjects = SceneManager.GetActiveScene().GetRootGameObjects();
+            for (int i = 0; i < RootObjects.Length; i++)
+            {
+                if (RootObjects[i].tag == "LevelObject" || RootObjects[i].tag == "DebugObject")
+                {
+                    
+                }
+                else
+                {
+                    RootObjects[i].transform.SetParent(LevelObject.transform);
+                }
+            }
+
+        }
 
 
     }
