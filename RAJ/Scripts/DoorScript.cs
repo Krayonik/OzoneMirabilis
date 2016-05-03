08d5680f7bc72e074ce5abed211754b049782512
PrithvirajGb <Prithviraj.g.b04@gmail.com>
2016-05-03 12:45:20 +0530

    Animation Update

---
+++ RAJ/Scripts/DoorScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorScript : MonoBehaviour 
+{
+
+	public Animator door;
+	public bool inSide;
+ 	public void DoorEnter(bool fromOut)
+	{
+		door.SetBool ("FromOutSide", fromOut);
+		door.SetTrigger ("Open");
+	}
+	public void DoorExit(bool fromOut)
+	{
+		door.SetBool ("FromOutSide", fromOut);
+		door.SetTrigger ("Close");
+	}
+ 	 
+
+}
---
+++ RAJ/Scripts/TriggerEvents.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerEvents : MonoBehaviour 
+{
+
+
+	DoorScript _door;
+	public bool outSide,forSetting;
+	void Start()
+	{
+		_door = transform.GetComponentInParent<DoorScript> ();
+	}
+	void OnTriggerEnter()
+	{
+		if (forSetting)
+			_door.inSide = outSide;
+		else
+			_door.DoorEnter (_door.inSide);
+	}
+	void OnTriggerExit()
+	{
+		if (forSetting)
+			_door.inSide = outSide;
+		else
+			_door.DoorExit (_door.inSide);
+	}
+
+}
